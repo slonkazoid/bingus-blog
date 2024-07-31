96922f2483a4edcec7b7ebfa6179f029905253b3
slonkazoid <slonkazoid@slonk.ing>
2024-08-01 01:10:08 +0300

    maybe fix race condition

--- static/date.js
+++ static/date.js
@@ -1,4 +1,5 @@
 for (let el of document.querySelectorAll(".date-rfc3339")) {
 	let date = new Date(Date.parse(el.textContent));
 	el.textContent = date.toLocaleString();
+	el.classList.remove("date-rfc3339");
 }
